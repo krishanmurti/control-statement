85b27fb9f55fefbac8c2409fe6691a2feb2005c0
krishanmurti <krishanmurti2003.in>
2024-08-07 22:30:00 +0530

    program

--- nice.java
+++ nice.java
@@ -1,5 +1,23 @@
+import java.util.*;
+
 public class nice {
     public static void main(String args[]) {
-        System.out.println(" hi i am krishna");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the button number:");
+        int button = sc.nextInt();
+        switch (button) {
+            case 1:
+                System.out.println("hello");
+                break;
+            case 2:
+                System.out.println("namaste");
+                break;
+            case 3:
+                System.out.println("bonjour");
+                break;
+            default:
+                System.out.println("invalid");
+        }
+
     }
 }
